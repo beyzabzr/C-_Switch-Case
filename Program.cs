5c3e5b9b583f02e70c57236d1e6330c50ea1044e
beyzabzr <113631271+beyzabzr@users.noreply.github.com>
2023-11-12 17:20:49 +0300

    main

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayindasiniz.");
+                    break;
+                case 2:
+                    Console.WriteLine("Subat ayindasiniz.");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayindasiniz.");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan ayindasiniz.");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayis ayindasiniz.");
+                    break;
+                case 6:
+                    Console.WriteLine("Haziran ayindasiniz.");
+                    break;
+                case 7:
+                    Console.WriteLine("Temmuz ayindasiniz.");
+                    break;
+                case 8:
+                    Console.WriteLine("Agustos ayindasiniz.");
+                    break;
+                case 9:
+                    Console.WriteLine("Eylul ayindasiniz.");
+                    break;
+                case 10:
+                    Console.WriteLine("Ekim ayindasiniz.");
+                    break;
+                case 11:
+                    Console.WriteLine("Kasim ayindasiniz.");
+                    break;
+                case 12:
+                    Console.WriteLine("Aralik ayindasiniz.");
+                    break;
+                default:
+                    Console.WriteLine("Yanlis Veri Girisi.");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kis Ayinasiniz.");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Ilk Bahar Ayindasiniz");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Ayindasiniz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Son Bahar Ayindasiniz");
+                    break;
+                default:
+                    Console.WriteLine("Yanlis Veri Girisi.");
+                    break;
+            }
+
+        }
+    }
+}
